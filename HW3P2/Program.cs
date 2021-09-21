d456c2302cf7a29d6471ca33a4f331386dbb0c10
joshuaUm <44787024+joshuaUm@users.noreply.github.com>
2021-09-20 21:41:07 -0700

    Adding main project files

---
+++ HW3P2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HW3P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isLampPlugged = true, isBulbBurnt = false; 
+
+
+            
+            Console.WriteLine("Lamp does not work.");
+
+            Console.WriteLine("Is lamp plugged in?");
+            if (isLampPlugged) 
+            {
+                Console.WriteLine("Yes, lamp is plugged in.");
+
+                Console.WriteLine("Is Bulb burnt out?");
+                if(isBulbBurnt)
+                {
+                    Console.WriteLine("Yes, replace light bulb.");
+                }
+                else
+                {
+                    Console.WriteLine("No, repair lamp.");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("No, plug lamp in.");
+            }
+            
+            
+
+
+
+
+
+        }
+    }
+}
+    
